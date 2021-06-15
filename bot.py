4f88014a9cdc2ae52b004fd3b2930fd93d5c39bd
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-06-15 14:39:06 +0500

    Bot created

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+import asyncio
 import os
 
-from aiogram import Bot, types
+import requests
+from aiogram import Bot, types, executor
 from aiogram.dispatcher import Dispatcher
 from dotenv import load_dotenv
 
@@ -8,6 +10,7 @@ load_dotenv()
 
 TOKEN = os.getenv('BOT_TOKEN')
 TELEGRAM_USER = os.getenv('TELEGRAM_USER_ID')
+PARTICIPANT = os.getenv('EXAM_PARTICIPANT')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -17,3 +20,24 @@ dp = Dispatcher(bot)
 async def process_start_command(message: types.Message):
     await message.reply('Bot started')
 
+
+async def check_page_update():
+    old_results = {}
+    while True:
+        print('Getting exams results....')
+        response = requests.get('http://checkege.rustest.ru/api/exam',
+                                cookies={'Participant': PARTICIPANT})
+        results = response.json()['Result']['Exams']
+        if old_results and results != old_results:
+            await bot.send_message(chat_id=TELEGRAM_USER, text='Результаты экзаменов изменились!')
+            print('Results changed')
+        old_results = results
+        await asyncio.sleep(60)
+
+
+async def start_checking_updates(x):
+    asyncio.create_task(check_page_update())
+
+
+if __name__ == '__main__':
+    executor.start_polling(dispatcher=dp, skip_updates=True, on_startup=start_checking_updates)

1ebb85c3bd5022e6d94466b29603994916e3d0c0
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-06-16 15:40:03 +0500

    Created add_participant function and changed example.env

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import asyncio
 import os
 
-import requests
 from aiogram import Bot, types, executor
 from aiogram.dispatcher import Dispatcher
 from dotenv import load_dotenv
@@ -9,8 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('BOT_TOKEN')
-TELEGRAM_USER = os.getenv('TELEGRAM_USER_ID')
-PARTICIPANT = os.getenv('EXAM_PARTICIPANT')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -18,39 +15,23 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply('Bot started')
+    await message.reply('To add participant, use /add command')
+
+
+@dp.message_handler(commands=['add'])
+async def add_participant(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id,
+                           text='Send your participant id (you can find it in '
+                                'https://checkege.rustest.ru/exams cookies)')
 
 
 @dp.message_handler(lambda msg: msg.text == 'ping')
-async def ping(msg: types.Message):
-    await bot.send_message(msg.from_user.id, 'pong')
+async def ping(message: types.Message):
+    await bot.send_message(message.from_user.id, 'pong')
 
 
 async def check_page_update():
-    await bot.send_message(chat_id=TELEGRAM_USER, text='Update checking started')
-    print('Update checking started')
-    old_results = {}
-    while True:
-        try:
-            print('Getting exams results....')
-            response = requests.get('http://checkege.rustest.ru/api/exam',
-                                    cookies={'Participant': PARTICIPANT})
-            results = response.json()['Result']['Exams']
-            changed_results = []
-            for result in results:
-                exam_id = result['ExamId']
-                old_exam = next(filter(lambda exam: exam['ExamId'] == exam_id, old_results), None)
-                if old_exam != result:
-                    changed_results.append(result['Subject'])
-            if old_results and results != old_results:
-                await bot.send_message(chat_id=TELEGRAM_USER,
-                                       text='Изменились результаты по предметам: {}'.format(
-                                           ', '.join(changed_results)))
-                print('Results changed', changed_results)
-            old_results = results
-        except Exception as e:
-            await bot.send_message(chat_id=TELEGRAM_USER, text='Error: {}'.format(e))
-        await asyncio.sleep(60)
+    pass
 
 
 async def start_checking_updates(x):
